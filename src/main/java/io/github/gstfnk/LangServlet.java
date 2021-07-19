15437bac1602c59bff89d62c23618a20ac23034a
gstfnk <gabrysia.stefaniak@gmail.com>
2021-07-19 22:44:30 +0200

    add dynamic lang msg

---
+++ src/main/java/io/github/gstfnk/LangServlet.java
@@ -0,0 +1,40 @@
+package io.github.gstfnk;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Lang", urlPatterns = {"/api/langs"})
+public class LangServlet extends HttpServlet {
+    private final Logger logger = LoggerFactory.getLogger(LangServlet.class);
+
+    private LangRepository repository;
+    private ObjectMapper mapper;
+
+    /**
+     * Servlet container needs it
+     */
+    @SuppressWarnings("unused")
+    public LangServlet() {
+        this(new LangRepository(), new ObjectMapper());
+    }
+
+    LangServlet(LangRepository repository, ObjectMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.info("Request from 2nd exercise with parameter " + req.getParameterMap());
+        resp.setContentType("application/json;charset=UTF-8");
+        mapper.writeValue(resp.getOutputStream(), repository.findAll());
+    }
+}
