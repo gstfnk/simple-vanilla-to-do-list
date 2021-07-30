45ee8a353379cfc7571bbaa3ae3b769040477464
gstfnk <gabrysia.stefaniak@gmail.com>
2021-07-30 13:34:05 +0200

    add doPost & doPut

--- src/main/java/io/github/gstfnk/todo/TodoServlet.java
+++ src/main/java/io/github/gstfnk/todo/TodoServlet.java
@@ -33,8 +33,29 @@ public class TodoServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        logger.info("Request TODO with parameter " + req.getParameterMap());
+        logger.info("Request TODO with parameter: " + req.getParameterMap());
         resp.setContentType("application/json;charset=UTF-8");
         mapper.writeValue(resp.getOutputStream(), repository.findAll());
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var pathInfo = req.getPathInfo();
+        try {
+            var todoId = Integer.valueOf(pathInfo.substring(1));
+            resp.setContentType("application/json;charset=UTF-8");
+            mapper.writeValue(resp.getOutputStream(), repository.toggleTodo(todoId));
+        } catch (NumberFormatException e) {
+            logger.warn("Wrong path used: " + pathInfo);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.info("Request POST got with parameters: " + req.getParameterMap());
+        Todo newTodo = mapper.readValue(req.getInputStream(), Todo.class);
+        logger.info(";;; " + newTodo.getText());
+        newTodo = repository.addTodo(newTodo);
+        mapper.writeValue(resp.getOutputStream(), newTodo);
     }
 }
